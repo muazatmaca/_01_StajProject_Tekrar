bf1255b25f68f109799c9eb80c8d63605f15971b
31635 <haticedere4844@gmail.com>
2023-04-13 13:12:15 +0200

    delete feature

--- src/test/java/FeatureFiles/_US_04_Hatice.feature
+++
@@ -1,1 +0,0 @@
-Feature: Parameters  Setup Functionality
--- src/test/java/Page/LeftNav.java
+++ src/test/java/Page/LeftNav.java
@@ -16,13 +16,13 @@ public class LeftNav extends Parent{
 
     // user story 1
     @FindBy(xpath = "//span[text()='Human Resources']")
-    public WebElement humanResources;
+    public WebElement humonResources;
 
     // humon resource un altındaki setup
     @FindBy(xpath = "(//span[text()='Setup'])[3]")
-    public WebElement humanResourscesSetup;
+    public WebElement humonResourscesSetup;
 
-    @FindBy(xpath = "(//span[text()='Position Categories'])[1]")
+    @FindBy(xpath = "")
     public WebElement positionCategories;
 
     @FindBy(xpath = "")
@@ -41,7 +41,7 @@ public class LeftNav extends Parent{
 
     // user story 4
     // diğer locaterlar ortak yukarı tarafı inceleyiniz
-    @FindBy(xpath = "")
+    @FindBy(xpath = "" )
     public WebElement fields;
 
     // user Story 5
@@ -107,8 +107,8 @@ public class LeftNav extends Parent{
         switch (strButton){
 
             case "setupButton":return setupButton;
-            case "humanResources":return humanResources;
-            case "humanResourscesSetup":return humanResourscesSetup;
+            case "humanResources":return humonResources;
+            case "humanResourscesSetup":return humonResourscesSetup;
             case "positionCategories":return positionCategories;
             case "attestations":return attestations;
             case "firstSetup":return firstSetup;
