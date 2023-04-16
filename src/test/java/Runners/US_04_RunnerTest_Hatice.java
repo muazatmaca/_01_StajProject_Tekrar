5ca79aa4a65bffc24b96373914661db136b2c46e
31635 <haticedere4844@gmail.com>
2023-04-16 13:25:25 +0200

    sanirim bitti

---
+++ src/test/java/Runners/US_04_RunnerTest_Hatice.java
@@ -0,0 +1,11 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/FeatureFiles/_US_04_Hatice.feature"},
+        glue = {"StepDefinitions"}
+)
+public class US_04_RunnerTest_Hatice extends AbstractTestNGCucumberTests {
+}
