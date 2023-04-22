4625d5467a0bbf49ec41afb03ee51249d3296aba
muazatmaca <muaz5447@gmail.com>
2023-04-22 06:54:12 +0300

    runner change Aynur hoca

--- src/test/java/Runners/US_03_Runner_Aynur.java
+++ src/test/java/Runners/US_03_Runner_Aynur.java
@@ -1,10 +1,11 @@
 package Runners;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles/US_03_Aynur.feature"},
         glue = {"StepDefinitions"})
 
-public class US_03_Runner_Aynur {
+public class US_03_Runner_Aynur extends AbstractTestNGCucumberTests {
 }
